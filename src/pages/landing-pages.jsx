eab872acb2d8ed5d46e7087c525b388e42afd638
Rahul Kumar <r1303yadav@gmail.com>
2024-04-08 12:58:33 +0530

    structure-folder

--- src/pages/landing-pages.jsx
+++ src/pages/landing-pages.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import AppLogo from "../assets/app-logo.svg";
 import { Link } from "react-router-dom";
-import "./css/index.css";
+import "../styles/landing-pages.css";
 
 function LandingPages() {
   return (
     <div className="nav-bar">
       <img src={AppLogo} className="app-logo" alt="logo" />
       <div className="nav-btns">
-        <Link to="/login" className="btn-login">Log in</Link>
+        <Link to="/login" className="btn-login">
+          Log in
+        </Link>
         <button className="btn-sign-up">Sign Up</button>
       </div>
     </div>
