a038610def17594a039f073e99c6186ec7b29342
Rahul Kumar <r1303yadav@gmail.com>
2024-04-08 12:26:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import LandingPages from "./pages/landing-pages.jsx";
+function App() {
+  return <LandingPages />;
+}
+
+export default App;
---
+++ src/pages/landing-pages.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AppLogo from "../assets/app-logo.svg";
+import { Link } from "react-router-dom";
+import "./css/index.css";
+
+function LandingPages() {
+  return (
+    <div className="nav-bar">
+      <img src={AppLogo} className="app-logo" alt="logo" />
+      <div className="nav-btns">
+        <Link to="/login" className="btn-login">Log in</Link>
+        <button className="btn-sign-up">Sign Up</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPages;
